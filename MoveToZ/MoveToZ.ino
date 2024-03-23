93b711beb5d0e4397cb094064abd05b76e84903c
SinCodigos <133539423+SinCodigos@users.noreply.github.com>
2024-03-22 17:53:04 -0700

    Update MoveToZ.ino

--- MoveToZ/MoveToZ.ino
+++ MoveToZ/MoveToZ.ino
@@ -1,6 +1,6 @@
 #include <AccelStepper.h>
 #define STEPS 200 // steps per rev for nema 17
-#define dir1 2 // Motor coil pin 1
+#define dir1 8 // Motor coil pin 1
 #define pul1 3 // Motor coil pin 2
 #define dir2 4
 #define pul2 5
@@ -11,17 +11,17 @@
 #define railLength 100000
 
   AccelStepper stepper1(1, pul1, dir1);
-  AccelStepper stepper2(1, pul2, dir2);
-  AccelStepper stepper3(1, pul3, dir3);
+  //AccelStepper stepper2(1, pul2, dir2);
+ // AccelStepper stepper3(1, pul3, dir3);
 
 void setup() {
 
-  stepper1.setMaxSpeed(200);
-  stepper1.setAcceleration(100);
-  stepper2.setMaxSpeed(200);
-  stepper2.setAcceleration(100);
-  stepper3.setMaxSpeed(200);
-  stepper3.setAcceleration(100);
+  stepper1.setMaxSpeed(1000);
+  stepper1.setAcceleration(1000);
+//  stepper2.setMaxSpeed(200);
+//  stepper2.setAcceleration(100);
+//  stepper3.setMaxSpeed(200);
+//  stepper3.setAcceleration(100);
   
 }
 
@@ -39,14 +39,24 @@ void setup() {
 
 void loop() {
 
-  stepper1.runToNewPosition(0);
-  stepper1.runToNewPosition(200);
-  stepper1.runToNewPosition(100);
+    // Move to position 0
+  stepper1.move(1000);
+  // stepper1.setSpeed(1000);
+  stepper1.runToPosition();
+ // stepper1.runToNewPosition(-1000);
+  delay(1000);
 
- // int targetZ = 100; // Example target z-coordinate
-  //moveToPosition(targetZ);
-  //delay(1000); // Delay for stability or do other operations
+  // Move to position 20000
+  stepper1.move(-1000);
+  // stepper1.setSpeed(-1000);
+  stepper1.runToPosition();
+ // stepper1.runToNewPosition(1000);
+  delay(1000);
 
-  // returnHome();
+  // Move back to position 100
+  //stepper1.moveTo(100);
+  //stepper1.runToPosition();
+ // delay(1000);
+
+//   returnHome();
 }
-
