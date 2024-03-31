ef8621734f89b6d5399c7fc223cbe0f00743dd88
kungfujunny <163086846+kungfujunny@users.noreply.github.com>
2024-03-31 16:28:56 -0700

    Update MoveToZ.ino

--- MoveToZ/MoveToZ.ino
+++ MoveToZ/MoveToZ.ino
@@ -1,63 +1,164 @@
+#include <Servo.h> //Arduino servo motor library
 #include <AccelStepper.h>
-#define STEPS 200 // steps per rev for nema 17
-#define dir1 8 // Motor coil pin 1
-#define pul1 3 // Motor coil pin 2
-#define dir2 4
-#define pul2 5
-#define dir3 6
-#define pul3 7
-#define homePositionC 0 // Home position on the rail
-#define homePositionA 0 // Home position for the arm
-#define railLength 100000
 
-  AccelStepper stepper1(1, pul1, dir1);
-  //AccelStepper stepper2(1, pul2, dir2);
- // AccelStepper stepper3(1, pul3, dir3);
+#define dir1 4 // Motor 1 direction, pin 2 doesnt work on MEGA
+#define pul1 3 // Motor 1 pulse 
+#define dir2 6 
+#define pul2 7
+
+//configure the servo motors
+Servo servo1;
+Servo servo2;
+AccelStepper stepper1(1, pul1, dir1);
+AccelStepper stepper2(1, pul2, dir2);
+
+bool xLocationStatus = false;
+bool yLocationStatus = false;
+bool zLocationStatus = false;
+
+const float pi = 3.1415926;
+const float L1 = 13; // Length between the first motor and the second motor (in cm)
+const float L2 = 11; // Length between the second motor and the end of arm (in cm)
+float x = -3; 
+float y = 5; 
+float z = 6;   
+float q1; // Angle of the first motor (in radian)
+float q1_deg; // Angle of the first motor (in degree)
+float q2; // Angle of the second motor (in radian)
+float q2_deg; // Angle of the second motor (in degree)
+float q; // Sum of in radian (q1+q2), it called theta
+float q_deg; // Sum of in degree (q1_deg + q2_deg)
+float x_sq ; // X square
+float y_sq ; // // Y square
+float L1_sq ; // L1 square
+float L2_sq ; // L2 square
+const int btn1 = 6;
+const int btn2 = 7;
+int btn1value = LOW; // For forward kinematics
+int btn2value = LOW; // For inverse kinematics
+
 
 void setup() {
-
-  stepper1.setMaxSpeed(50000);
-  stepper1.setAcceleration(10000);
-//  stepper2.setMaxSpeed(200);
-//  stepper2.setAcceleration(100);
-//  stepper3.setMaxSpeed(200);
-//  stepper3.setAcceleration(100);
   
+servo1.attach(8);
+servo2.attach(9);
+
+stepper1.setMaxSpeed(500000);
+stepper1.setAcceleration(100000);
+stepper2.setMaxSpeed(500000);
+stepper2.setAcceleration(100000);
+
+
+Serial.begin(9600); 
+
+  Serial.print("Theta1: ");
+  Serial.print(q1_deg); 
+  Serial.print(" degrees, Theta2: ");
+  Serial.print(q2_deg); // 
+  Serial.println(" degrees");
+  Serial.print("Theta: ");
+  Serial.print(q_deg); // 
+  Serial.println(" degrees");
+  delay(1000);
+
 }
 
-// void moveToPosition(int targetZ) {
-
-//int stepsToMove = (targetZ - stepper.currentPosition()) * STEPS / railLength;
-  //stepper.step(stepsToMove);
-
-//}
-
-//void returnHome() {
- // moveToPosition(homePositionC);
-//}
-
-
 void loop() {
+  
+  q = atan2(y,x);
+  
+  x_sq = pow(x,2); 
+  y_sq = pow(y,2); 
+  L1_sq = pow(L1,2); 
+  L2_sq = pow(L2,2); 
+  
+  q1 = acos((x_sq + y_sq - (L1_sq + L2_sq))/(2*L1*L2)); 
+  q2 = q - q1 ; 
+  
+  q1_deg = q1*180/pi ; // convert the angle from radian to degree
+  q2_deg = q2*180/pi; // convert the angle from radian to degree
+  q_deg = q*180/pi; 
+  //servo1.write(120-q1_deg); 
+  //servo2.write(80-q2_deg); 
+  servo1.write(120); 
+  servo2.write(80); 
 
-    // Move to position 0
-  stepper1.move(50);
-  // stepper1.setSpeed(1000);
-  stepper1.runToPosition();
- // stepper1.runToNewPosition(-1000);
-  //delay(1000);
 
-  // Move to position 20000
-  stepper1.move(-100);
-  // stepper1.setSpeed(-1000);
-  stepper1.runToPosition();
- 
- // stepper1.runToNewPosition(1000);
- // delay(1000);
 
-  // Move back to position 100
-  //stepper1.moveTo(100);
-  //stepper1.runToPosition();
- // delay(1000);
+  moveJ1(z);
+  moveJ2(x);
+  J2Home(x); 
 
-//   returnHome();
+
+}
+
+void moveJ2(float x){
+int a = 100;
+int pos = 0;
+
+if (x=0){
+
+pos = 0; 
+stepper2.move(pos);
+stepper2.runToPosition();
+
+}
+
+else if(x<0){
+pos = -200*a;
+stepper2.move(pos);
+stepper2.runToPosition();
+
+}
+
+else {
+pos = 200*a;
+stepper2.move(pos);
+stepper2.runToPosition();
+
+}
+
+return;
+
+}
+
+void moveJ1(float z){
+
+int a = 100; 
+int z_conv = z*200*a;
+
+stepper1.move(z_conv);
+stepper1.runToPosition();
+
+return; 
+
+}
+
+void J2Home(float x){
+int pos = 0; 
+int a = 100; 
+
+  if (x=0){
+    pos = 0; 
+    stepper2.move(pos);
+    stepper2.runToPosition();
+
+  }
+
+  else if(x<0){
+    pos = 200*a;
+    stepper2.move(pos);
+    stepper2.runToPosition();
+
+  }
+
+  else {
+    pos = -200*a;
+    stepper2.move(pos);
+    stepper2.runToPosition();
+
+  }
+
+  return;
+
 }
