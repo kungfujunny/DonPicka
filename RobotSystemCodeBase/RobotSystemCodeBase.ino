7a32cefb7ea2bb8112ebaffd600e2cb08b7c63e7
SinCodigos <133539423+SinCodigos@users.noreply.github.com>
2024-04-07 19:26:41 -0700

    Modified GUI, added Home Functionality, minor error fixes
    Condensed user text input into a single input, added Home and Sethome functions, Optimized movement part of the code so that it only runs when the target position is updated.
    
    NEEDED CHANGES
    Modify movement code because currently it just repeats the same movement backwards after a 5sec delay. Target moving to position and staying there, only moving back if a new position is inputted or if home is inputted. Remove duplicate movement for loops by creating a single function to stream line code.

--- RobotSystemCodeBase/RobotSystemCodeBase.ino
+++ RobotSystemCodeBase/RobotSystemCodeBase.ino
@@ -13,9 +13,13 @@ Servo servo3;
 AccelStepper stepper1(1, pul1, dir1);
 AccelStepper stepper2(1, pul2, dir2);
 
-bool xLocationStatus = false;
-bool yLocationStatus = false;
-bool zLocationStatus = false;
+//bool xLocationStatus = false;
+//bool yLocationStatus = false;
+//bool zLocationStatus = false;
+
+int homeX = 0;
+int homeY = 0;
+int homeZ = 0;
 
 void setup() {
 
@@ -30,7 +34,7 @@ void setup() {
   stepper2.setAcceleration(103000);
 
   Serial.begin(9600);
-  int xPosition;
+  //int xPosition;
   
 }
 
@@ -43,16 +47,22 @@ void loop() {
   servo3.write(angleServo3);
   int angle2 = 180-alpha;
   servo2.write(angle2);
-  
-  int xPosition = int(xpos());
 
-  Serial.print("The Robot will move to: ");
+  //
+  int xPosition, yPosition, zPosition;
+  bool positionUpdate = getPositions(xPosition, yPosition, zPosition);
+  if(!positionUpdate) return;
+  //
+  
+  //int xPosition = int(xpos());
+
+  /*Serial.print("The Robot will move to: ");
   Serial.print(xPosition);
   Serial.println("cm");
-  
+  */
   stepper1.move(-xPosition*400);
   stepper1.runToPosition();
-
+  /*
   int yPosition = int(ypos());
   Serial.print("The Robot will move to: ");
   Serial.print(yPosition);
@@ -62,9 +72,10 @@ void loop() {
   Serial.print("The Robot will move to: ");
   Serial.print(zPosition);
   Serial.println("cm");
-
+  */
+  
   int hyp = sqrt(sq(yPosition)+sq(zPosition));
-  Serial.println(yPosition);
+  //Serial.println(yPosition);
   
   if(hyp>=17 && hyp <=40){
     int deltaLength = hyp-17;
@@ -153,7 +164,51 @@ void loop() {
 
 
 }
+bool getPositions(int &xPos, int &yPos, int &zPos) {
+  Serial.println("Enter X, Y, Z positions separated by commas, 'sethome', or 'home'");
+  while (Serial.available() == 0) {}
 
+  String input = Serial.readStringUntil('\n');
+  if(input == "sethome"){
+     homeX = xPos;
+     homeY = yPos;
+     homeZ = zPos;
+     Serial.println("Home position set.");
+     return false;
+  }
+  else if(input == "home"){
+    xPos = homeX;
+    yPos = homeY;
+    zPos = homeZ;
+    Serial.println("Moving to home position.");
+     return true;
+  }
+  int commaIndex1 = input.indexOf(',');
+  int commaIndex2 = input.lastIndexOf(',');
+
+  if (commaIndex1 == -1 || commaIndex2 == -1 || commaIndex1 == commaIndex2) {
+    Serial.println("Invalid input. Please use the format X,Y,Z");
+    return false;
+  }
+
+  String xStr = input.substring(0, commaIndex1);
+  String yStr = input.substring(commaIndex1 + 1, commaIndex2);
+  String zStr = input.substring(commaIndex2 + 1);
+
+  xPos = xStr.toInt();
+  yPos = yStr.toInt();
+  zPos = zStr.toInt();
+
+  Serial.print("Moving to position (X,Y,Z): ");
+  Serial.print(xPos);
+  Serial.print(",");
+  Serial.print(yPos);
+  Serial.print(",");
+  Serial.println(zPos);
+  return true;
+}
+
+/*
 int xpos() {
   // Ask for the X position
   Serial.println("Please enter the X position (less than 50cm:");
@@ -218,3 +273,4 @@ int zpos() {
   }
   return zPos;
 }
+*/
